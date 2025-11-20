569fb5a726b759f0afded168dbf0ba653c4adbcb
Lakshya Sharma <lakshya4568@gmail.com>
2025-11-21 02:06:21 +0530

    Remove outdated webpack rules for asset handling
    Removed obsolete rules for pdfjs-dist and other assets.

--- webpack.rules.ts
+++ webpack.rules.ts
@@ -30,42 +30,4 @@ export const rules: Required<ModuleOptions>["rules"] = [
       },
     },
   },
-  // Fix for pdfjs-dist ESM compatibility
-  {
-    test: /[/\\]node_modules[/\\]pdfjs-dist[/\\]/,
-    type: "javascript/auto",
-  },
-  // Bundle PDF worker as a local asset
-  {
-    test: /pdf\.worker\.(min\.)?m?js$/,
-    type: "asset/resource",
-    generator: {
-      filename: "pdf.worker.[contenthash].mjs",
-    },
-  },
-  {
-    test: /\.tsx?$/,
-    exclude: /(node_modules|\.webpack)/,
-    use: {
-      loader: "ts-loader",
-      options: {
-        transpileOnly: true,
-        compilerOptions: {
-          module: "commonjs",
-          noEmit: false,
-        },
-      },
-    },
-  },
-  {
-    test: /\.css$/,
-    use: ["style-loader", "css-loader", "postcss-loader"],
-  },
-  {
-    test: /\.(woff|woff2|eot|ttf|otf)$/i,
-    type: "asset/resource",
-    generator: {
-      filename: "fonts/[name][ext]",
-    },
-  },
-];
+  
